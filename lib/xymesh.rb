bab9bdd6004748c3a766b59c10cc875f27dd0bc0
Dmitri Priimak <priimak@gmail.com>
2016-03-24 16:43:52 -0700

    Removed more trailing whitespace. We are out there in 3D.
    Change-Id: Ib21d1952d2b90affd64101077bc81967f8c9d34c

--- lib/xymesh.rb
+++ lib/xymesh.rb
@@ -1,5 +1,5 @@
 # Open class Array and add methods 'abs' and 'cross_product'
-class Array 
+class Array
   # Calculate absolute value of the vector represented by this arrary
   def abs
     val=0
@@ -39,15 +39,15 @@ module XYMesh
   require "xymesh_aux"
 
   # This class represents two dimensional grid of points in the X-Y plane.
-  # It performs externally provieded computations at these points and refines 
+  # It performs externally provieded computations at these points and refines
   # grid as nessesary by intorucing new points.
-  class Grid2D
-    
-    # @return [Proc] external function that takes X,Y coordinates as parameters and return Z-value 
+  class Grid3D
+
+    # @return [Proc] external function that takes X,Y coordinates as parameters and return Z-value
     # evaluated at this point.
     attr_accessor :computer
 
-    # @return [LList] linked list of tiles. 
+    # @return [LList] linked list of tiles.
     # @see Tile
     attr_accessor :tiles
 
@@ -55,11 +55,11 @@ module XYMesh
     # @see Vertex
     attr_accessor :vtxs
 
-    # This accessor can be use to set maximum number of vertexes that can 
+    # This accessor can be use to set maximum number of vertexes that can
     # exist in the mesh, which limits number of calculatons to be performed.
-    # Default value is nil, which means that vertixes will be created 
+    # Default value is nil, which means that vertixes will be created
     # indefinitely until conditions for their creation cease to exist.
-    # @return [Fixnum] number indicating maximim number of vertixes 
+    # @return [Fixnum] number indicating maximim number of vertixes
     attr_accessor :max_vertexes
 
     # return [Float] minimum area of the tile projected to X-Y plance in percentage from 0 to 1
@@ -69,8 +69,8 @@ module XYMesh
     attr_reader :min_area_xy
 
 
-    # return [Proc] function which is called after each iteration in refine_recursively() and 
-    # right after compute(). This function recieves refefence to this class (Grid2D) and a 
+    # return [Proc] function which is called after each iteration in refine_recursively() and
+    # right after compute(). This function recieves refefence to this class (Grid2D) and a
     # number of newly created vertexes
     attr_accessor :iter_callback
 
@@ -85,7 +85,7 @@ module XYMesh
     # @param y_max [Float] maximum value along the Y-axis
     # @param n_y [Fixnum] number of segments along the Y-axis, which gives n_y+1 points.
     # @param computer [Proc] external function to be evaluated at each point in the grid
-    def initialize(x_min, x_max, n_x, y_min, y_max, n_y, computer=nil) 
+    def initialize(x_min, x_max, n_x, y_min, y_max, n_y, computer=nil)
       @x_min = x_min
       @x_max = x_max
       @n_x   = n_x
@@ -128,7 +128,7 @@ module XYMesh
 
     # Errors in numerical simulations may lead to small errors/ripples on the generated surface
     # which can lead to indefinite recursion once the size of the tiles becomes comparable to
-    # size of the ripples. To prevent this from happening you can set this variable to set minimum 
+    # size of the ripples. To prevent this from happening you can set this variable to set minimum
     # area of projected to X-Y plane newly generated tiles in percentage from 0 to 1 of original,
     # starting tiles.
     # @return [Float] value that has been set
@@ -152,9 +152,9 @@ module XYMesh
       @tiles.each { |i,t| return i if t.value == tile }
     end
 
-    # Perform mesh refinement by adding new vertex points to the tiles, curvature for 
-    # which exceeds provied max_curvature value. To reach desired smoothness you need 
-    # repeatedly call this function until amount of tiles that were split (returned by 
+    # Perform mesh refinement by adding new vertex points to the tiles, curvature for
+    # which exceeds provied max_curvature value. To reach desired smoothness you need
+    # repeatedly call this function until amount of tiles that were split (returned by
     # this function ) is zero. Alternatively you can call ''
     #
     # @param max_curvature [Float] curvature value below which we perform mesh refinement
@@ -163,10 +163,10 @@ module XYMesh
       @nvariance_limit = max_curvature
       refined = 0
       tiles_to_split=[]
-      @tiles.each { |i, t| 
+      @tiles.each { |i, t|
         nvr = t.value.nvariance()
-        if nvr[1] > max_curvature 
-          # Something needs to be split, either this tile or its counterpart which gives 
+        if nvr[1] > max_curvature
+          # Something needs to be split, either this tile or its counterpart which gives
           # us highest nvariance. So we will split the one that has largest perimeter
           if nvr[0].perimeter > t.value.perimeter
             tiles_to_split << nvr[0]
@@ -177,7 +177,7 @@ module XYMesh
       }
       tiles_to_split.each { |t|
         if t.inspect? and t.area_vector()[2].abs > @min_area_xy
-          t.split_and_evaluate() 
+          t.split_and_evaluate()
           refined = refined + 1
         end
         return 0 if not @max_vertexes.nil? and @vtxs.size >= @max_vertexes
@@ -185,10 +185,10 @@ module XYMesh
       refined
     end # of refine(...)
 
-    # Same as 'refine(max_curvature)', but will contain recursively until maximum tile 
+    # Same as 'refine(max_curvature)', but will contain recursively until maximum tile
     # curvature falls bellow max_curvature
     # @return [Grid2D] self
-    def refine_recursively(max_curvature) 
+    def refine_recursively(max_curvature)
       refined = self.refine(max_curvature)
       while refined > 0
         if not @iter_callback.nil?
@@ -197,7 +197,7 @@ module XYMesh
 
         refined = self.refine(max_curvature)
       end
-      
+
       if not @iter_callback.nil?
         # last callback call should have refined=0
         @iter_callback.call(self, refined)
@@ -207,9 +207,9 @@ module XYMesh
     end
 
     # @return [Float] current maximum curvature among all the tiles
-    def max_nvariance() 
+    def max_nvariance()
       max_curvature = 0
-      @tiles.each { |i, t| 
+      @tiles.each { |i, t|
         c = t.value.nvariance()
         max_curvature = c[1] if c[1] > max_curvature
       }
@@ -239,10 +239,10 @@ module XYMesh
       retval
     end
 
-    # Will save safely generated model to a file named 'fname' by writing first to a tmp 
+    # Will save safely generated model to a file named 'fname' by writing first to a tmp
     # file named '#{fname}.tmp' and then moving over to 'fname'
     # @return [Grid2D] self
-    def save_safe_to_java3d_obj_file(fname) 
+    def save_safe_to_java3d_obj_file(fname)
       tmp_file_name = "#{fname}.tmp"
       File.open(tmp_file_name, "w") { |file|
         file.write("# xymesh nvariance_limit #{@nvariance_limit.to_s}\n")
@@ -266,8 +266,8 @@ module XYMesh
         spll = l.split()
         if spll.size == 4
           case spll[0]
-          when "#" 
-            @nvariance_limit = spll[3].to_f if spll[1] == "xymesh" and spll[2] == "nvariance_limit" 
+          when "#"
+            @nvariance_limit = spll[3].to_f if spll[1] == "xymesh" and spll[2] == "nvariance_limit"
 
           when "v"
             # this is a vertex
@@ -302,19 +302,19 @@ module XYMesh
   # A tile is a flat triangular surface defined by three vertices.
   # All tiles are stored in the LNode in the linked list LList
   class Tile
-    
+
     # @return [Array] Array of three Vertexes forming this tile
     attr_accessor :vtx
 
-    # @return [TrueClass,FalseClass] parameter to be used 
+    # @return [TrueClass,FalseClass] parameter to be used
     # internally indicating if this tile should be inspected for splitting
     attr_accessor :inspect
 
-    @lnode 
+    @lnode
 
     # @param vtx1 vtx2 vtx3 [Vertex] three verixes forming tile.
     # @param grd [Grid2D] gid, which this tile is part of
-    def initialize(vtx1,vtx2,vtx3, grd) 
+    def initialize(vtx1,vtx2,vtx3, grd)
       @vtx=[]
       raise "Invalid vtx1 type" if vtx1.instance_of? Vertex
       raise "Invalid vtx2 type" if vtx2.instance_of? Vertex
@@ -331,14 +331,14 @@ module XYMesh
 
       # regsiter this tile with each vertex that forms it
       @vtx.each { |v| @grd.vtxs[v-1].register_tile(self) }
-      
+
       @inspect=true
       @lnode = nil
     end
 
     def perimeter
       return @prm unless @prm.nil?
-      @prm = 
+      @prm =
         @grd.vtxs[vtx[0]-1].minus(@grd.vtxs[vtx[1]-1]).abs +
         @grd.vtxs[vtx[0]-1].minus(@grd.vtxs[vtx[2]-1]).abs +
         @grd.vtxs[vtx[1]-1].minus(@grd.vtxs[vtx[2]-1]).abs
@@ -346,8 +346,8 @@ module XYMesh
 
     # Find all nearby tiles connected to this one through the common edges ( up to three tiles )
     # and for each tile calculate cross product between normal vectors of this tile and nearby ones.
-    # Find the one that give maximum of these three cross products by absolute value and return array 
-    # where first element is adjacent tile that gives us maximum nvariance and second element is 
+    # Find the one that give maximum of these three cross products by absolute value and return array
+    # where first element is adjacent tile that gives us maximum nvariance and second element is
     # the nvariance. This will be our rough measure of curvature of surface formed this set of tiles.
     def nvariance(debug=false)
       nv0 = self.normal_vector
@@ -365,14 +365,14 @@ module XYMesh
 
       all_near_by_tiles.map { |t| [t, nv0.cross_product(t.normal_vector).abs] }.sort { |a,b| a[1] <=> b[1] }.last
     end
-    
+
     # Create back reference to LNode in which this tile is a payload.
     def connect(lnode)
       @lnode = lnode
     end
 
     # Remove from linked list of tiles
-    def unlink 
+    def unlink
       @lnode.unlink unless @lnode.nil?
     end
 
@@ -382,7 +382,7 @@ module XYMesh
     end
 
     def area_vector
-      return @av unless @av.nil? 
+      return @av unless @av.nil?
 
       r1x=@grd.vtxs[vtx[1]-1].x-@grd.vtxs[vtx[0]-1].x
       r1y=@grd.vtxs[vtx[1]-1].y-@grd.vtxs[vtx[0]-1].y
@@ -400,7 +400,7 @@ module XYMesh
 
     # Form normal vector to this tile if needed and return it.
     def normal_vector
-      return @nv unless @nv.nil? 
+      return @nv unless @nv.nil?
 
       nv = self.area_vector
       length = nv.abs
@@ -422,14 +422,14 @@ module XYMesh
       self.split(true)
     end
 
-    # If curvature returned by nvariance() is greater then some prescribed value 
-    # then this method can be called to split this tile by adding new vertex and 
+    # If curvature returned by nvariance() is greater then some prescribed value
+    # then this method can be called to split this tile by adding new vertex and
     # optionally evaluate value at that new vertex
     def split(evaluate=false)
       @inspect = false # no more ispection for this tile
 
-      l=[0,1,2].map { |m| 
-        ((m+1)..2).to_a.map { |n| 
+      l=[0,1,2].map { |m|
+        ((m+1)..2).to_a.map { |n|
           (@grd.vtxs[@vtx[m]-1].x-@grd.vtxs[@vtx[n]-1].x)**2+(@grd.vtxs[@vtx[m]-1].y-@grd.vtxs[@vtx[n]-1].y)**2
         }
       }.flatten.each_with_index.max[1]
@@ -454,10 +454,10 @@ module XYMesh
         vtx3=vtx[0]
       end
 
-      vtx_new = 
+      vtx_new =
         Vertex.new((@grd.vtxs[vtx1-1].x+@grd.vtxs[vtx2-1].x)/2.0, (@grd.vtxs[vtx1-1].y+@grd.vtxs[vtx2-1].y)/2.0, @grd)
 
-      vtx_new.compute() if( evaluate ) 
+      vtx_new.compute() if( evaluate )
 
       @grd.vtxs << vtx_new
 
@@ -481,7 +481,7 @@ module XYMesh
 
         t = Tile.new(vtx4,vtx2,@grd.vtxs.size, @grd)
         t.connect(@grd.tiles.append(t))
-        
+
         # unlink ajd_tile from the list
         adj_tile.unlink
         adj_tile.inspect = false
@@ -489,9 +489,9 @@ module XYMesh
         @grd.vtxs[vtx2-1].remove_tile adj_tile
         @grd.vtxs[vtx4-1].remove_tile adj_tile
       end
-      
+
       self.unlink
-      
+
       @grd.vtxs[vtx1-1].remove_tile self
       @grd.vtxs[vtx2-1].remove_tile self
       @grd.vtxs[vtx3-1].remove_tile self
@@ -504,12 +504,12 @@ module XYMesh
 
   end # of class Tile
 
-  # This class represents point in the grid. In the normal course of events you do not have 
+  # This class represents point in the grid. In the normal course of events you do not have
   # explicitly create instances of this class.
-  class Vertex 
+  class Vertex
 
     attr_accessor :x, :y, :value, :initialized, :tiles
-    
+
     # @param x [Float] x coordinate of the Vertex
     # @param y [Float] x coordinate of the Vertex
     # @param grd [Grid2D] reference to a grid object
@@ -524,7 +524,7 @@ module XYMesh
       @tiles = []
     end
 
-    # If value has never been computed then such vertex considered to be 
+    # If value has never been computed then such vertex considered to be
     # not initialized. This method allows to query for such condition.
     def initialized?
       initialized
@@ -537,8 +537,8 @@ module XYMesh
       self
     end
 
-    # Remove tile connected to this vertex. Such operation is done when 
-    # mesh refinement is performed, which results in new vertexes introduced 
+    # Remove tile connected to this vertex. Such operation is done when
+    # mesh refinement is performed, which results in new vertexes introduced
     # and tiles split into several new tiles.
     def remove_tile(tile)
       @tiles.reject! { |t| t == tile }
